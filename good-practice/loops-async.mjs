1cf6918651f565e4d7ca378828946300230b8af4
david123456858 <juandavidperaltafuentes@gmail.com>
2025-06-27 17:06:38 -0500

    implementando algo nuevo pero no funciona

--- good-practice/loops-async.mjs
+++ good-practice/loops-async.mjs
@@ -24,8 +24,8 @@ const fetchGetId = async ()=>{
 
 /**
  * es muy interesante ya que con esto podemos realizar en este caso muchas busquedas por blques
- * y esperar que termine las promesas y mandarlas de unoa en este caso se recorre el array con 
- * el metodo map para devovler un array de promesas no resueltas se verias algo aso
+ * y esperar que termine las promesas y mandarlas de una, en este caso se recorre el array con 
+ * el metodo map para devovler un array de promesas no resueltas se verias algo así
  * [Promises {<pending>}] es cada uno de los array pero llega el Promises luego de haber
  * creado cada uno de las promesas, esta las ejectua para mayor velocidad en vez de ejecutar
  * una por una lo hace todo en paralelo
@@ -38,7 +38,7 @@ const getIdLoopPromiseAll = async ()=>{
 
 /**
  * 
- * @returns {Promise.allSettled(result)}
+ * @returns { Promise.allSettled(result) }
  * y bueno en algunos casos (si no en todos) al aparecer dura mucho menos el utilizarlo
  * realmente la comparativa es muy interesante de observar en este pequeño ejemplo
  */
@@ -46,15 +46,28 @@ const getIdLoopPromiseAll = async ()=>{
 
 /**
  * 
- * @returns {Comparativa}
+ * @returns { Comparativa }
  * paralelo: 267.093ms --> resolviendo promesas en paraleo con Promise.all
  * uno: 363.964ms --> resolviendo promesa por promesa
  * paralelo2: 68.239ms --> resolviendo promesas en paraleo con Promise.allSettled
  */
 const getIdLoop = async ()=>{
-    const result = usersId.map(id => getUserId(id))
-    const users =  await Promise.allSettled(result)
+    const users =  await Promise.allSettled(
+        usersId.map( id =>  getUserId(id))
+    )
     return users
+}
+
+
+/**
+ * JAJAJAJ lo documento porque no estoy termiando de entender este tema
+ * y sale algo mas nuevo, bueno hay una alternativa para el promise.All()
+ * es una nueva actualizacino para resolver las promesas en conjunto
+ * llamada Array.fromAsync() PERO NO FUNCIONA TODAVIA
+ */
+const getArray = async ()=>{
+    const arr = Array.fromAsync(usersId.map( id =>  getUserId(id)))
+    return arr
 }
 
 console.time('paralelo')
